46df8151c7efbdb5b776bed5b822ce740480c6d8
Bramwel <bramwel.mutngati@student.moringaschool.com>
2025-03-28 10:44:21 +0300

    fetch update

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,70 +1,69 @@
-fetch('products.json') 
-    .then(response => response.json())
-    .then(products => {
+fetch('http://localhost:3000/products')
 
-        let productsHTML = '';
+    
+let productsHTML = '';
 
-        products.forEach((product) => {
-            productsHTML += `
-                <div class="product-container">
-                    <div class="product-image-container">
-                        <img class="product-image" src="${product.image}">
-                    </div>
-        
-                <div class="product-name limit-text-to-2-lines">
-                    ${product.name}
-                </div>
-        
-                <div class="product-rating-container">
-                    <img class="product-rating-stars"
-                    src="images/ratings/rating-${product.rating.stars * 10}.png">
-                    <div class="product-rating-count link-primary">
-                        ${product.rating.count}
-                    </div>
-                </div>
-        
-                <div class="product-price">
-                    $${(product.priceCents / 100).toFixed(2)}
-                </div>
-        
-                <div class="product-quantity-container">
-                    <select>
-                        ${Array.from({ length: 10 }, (_, i) => `<option value="${i + 1}">${i + 1}</option>`).join('')}
-                    </select>
-                </div>
-        
-                <div class="product-spacer"></div>
-        
-                <div class="added-to-cart">
-                    <img src="images/icons/checkmark.png">
-                    Added
-                </div>
-        
-                <button class="add-to-cart-button button-primary js-add-to-cart"
-                data-product-id="${product.name}">
-                    Add to Cart
-                </button>
-            </div>}`;
-        `;
+products.forEach((product) => {
+    productsHTML += `
+        <div class="product-container">
+            <div class="product-image-container">
+                <img class="product-image" src="${product.image}">
+            </div>
+
+        <div class="product-name limit-text-to-2-lines">
+            ${product.name}
+        </div>
+
+        <div class="product-rating-container">
+            <img class="product-rating-stars"
+            src="images/ratings/rating-${product.rating.stars * 10}.png">
+            <div class="product-rating-count link-primary">
+                ${product.rating.count}
+            </div>
+        </div>
+
+        <div class="product-price">
+            $${(product.priceCents / 100).toFixed(2)}
+        </div>
+
+        <div class="product-quantity-container">
+            <select>
+                ${Array.from({ length: 10 }, (_, i) => `<option value="${i + 1}">${i + 1}</option>`).join('')}
+            </select>
+        </div>
+
+        <div class="product-spacer"></div>
+
+        <div class="added-to-cart">
+            <img src="images/icons/checkmark.png">
+            Added
+        </div>
+
+        <button class="add-to-cart-button button-primary js-add-to-cart"
+        data-product-id="${product.name}">
+            Add to Cart
+        </button>
+    </div>
+`;
+});
+
+document.querySelector('.js-products-grid').innerHTML = productsHTML;
+
+document.querySelectorAll('.js-add-to-cart').forEach((button) => {
+    button.addEventListener('click', () => {
+        const productName = button.dataset.productId;
+
+        let matchingItem = cart.find(item => item.productName === productName);
+
+    if (matchingItem) {
+        matchingItem.quantity += 1;
+    } else {
+        cart.push({
+            productName: productName,
+            quantity: 1
         });
-        
-        document.querySelector('.js-products-grid').innerHTML = productsHTML;
-        
-        document.querySelectorAll('.js-add-to-cart').forEach((button) => {
-            button.addEventListener('click', () => {
-                const productName = button.dataset.productId;
-        
-                let matchingItem = cart.find(item => item.productName === productName);
-        
-            if (matchingItem) {
-                matchingItem.quantity += 1;
-            } else {
-                cart.push({
-                    productName: productName,
-                    quantity: 1
-                });
-            }
-        
-            console.log(cart);
-        });
-        });
+    }
+
+    console.log(cart);
+});
+});
